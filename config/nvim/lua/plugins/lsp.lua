22c21503fbf06804d68c285c6202a689adf5ed47
Jonathan Olsson <jonathan@babar.lan>
2024-10-20 14:50:09 +0200

    add nvim configuration

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "biome",
+                    "lua_ls",
+                    "ts_ls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.ts_ls.setup({})
+            lspconfig.biome.setup({})
+        end,
+    },
+}
---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        keys = {
+            { "<leader>pf", function() require("telescope.builtin").find_files() end, desc = "Telescope find files" },
+            { "<C-p>", function() require("telescope.builtin").git_files() end, desc = "Telescope find git files" },
+            { "<leader>ps", function() require("telescope.builtin").grep_string({search = vim.fn.input("Grep > ") }) end, desc = "Telescope find files" }
+        }
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            })
+            require("telescope").load_extension("ui-select")
+        end
+    }
+}
